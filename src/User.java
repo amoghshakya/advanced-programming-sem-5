65d62ea67303b7990982923e42aeb61566a601d6
Amogh Shakya <amoghshakya@outlook.com>
2024-05-17 16:58:41 +0545

    Weekly Assignment 1

---
+++ src/User.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+
+public class User {
+    private String fullName;
+    private String mobileNumber;
+    private String password;
+    private String DOB;
+
+    public User(String fullName, String mobileNumber, String password, String DOB) {
+        this.fullName = fullName;
+        this.mobileNumber = mobileNumber;
+        this.password = password;
+        this.DOB = DOB;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "fullName='" + fullName + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", DOB=" + DOB +
+                '}';
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
